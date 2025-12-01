eee08aefafa138fe763ad1431f881b9283ab594d
Taysa Abinader <ferreira.taysa@gmail.com>
2025-12-01 19:06:30 +0200

    [Iter 4] Part 1: Update a job (Edit View)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 
 // pages & components
+import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/HomePage";
 import AddJobPage from "./pages/AddJobPage";
 import JobPage from "./pages/JobPage";
-import Navbar from "./components/Navbar";
-import NotFoundPage from "./pages/NotFoundPage"
+import EditJobPage from "./pages/EditJobPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/add-job" element={<AddJobPage />} />
+        <Route path="/edit-job/:id" element={<EditJobPage />} />
+        <Route path="/jobs/:id" element={<JobPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
 
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/add-job" element={<AddJobPage />} />
-              <Route path="/jobs/:id" element={<JobPage />} />
-              <Route path='*' element={<NotFoundPage />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
-  
-  export default App;
+  return <RouterProvider router={router} />;
+};
+
+export default App;
